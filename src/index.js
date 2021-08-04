739069e3a57afb930a30d6d85b65ce5de2cff0a1
shakhzod2003 <yshahzod0@gmail.com>
2021-08-04 15:41:44 +0500

    new commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Studets from './App';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Studets />
+    <Studets title='name is Shahzod.'/>
+    <Studets title='surname is Yoqubov.'/>
+    <Studets title='status is student.'/>
+    <Studets title='ID is 21072003.'/>
+    <Studets title='plan is to become a perfect web-developer.'/>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
